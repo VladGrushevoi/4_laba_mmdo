eeef400b37f69f3f260243202e45361e260f9cd2
VladGrushevoi <vlad.grush.99@gmail.com>
2019-11-08 21:45:44 +0200

    Добавьте файлы проекта.

---
+++ 4_laba_mmdo/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace _4_laba_mmdo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.x = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(374, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Побудувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(180, 12);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            -2147483648});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(180, 40);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(30, 106);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1140, 502);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ліва межа";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Права межа";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Алгоритм пошуку відрізка локалізації точки мінімуму",
+            "Алгоритм методу дихотомії",
+            "Алгоритм методу золотого перерізу",
+            "Алгоритм методу Фібоначч",
+            "Алгоритм методу парабол"});
+            this.comboBox1.Location = new System.Drawing.Point(180, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(380, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "Виберіть метод";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Вибір метода";
+            // 
+            // x
+            // 
+            this.x.AutoSize = true;
+            this.x.Location = new System.Drawing.Point(654, 42);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(46, 17);
+            this.x.TabIndex = 8;
+            this.x.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 626);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label x;
+    }
+}
+
---
+++ 4_laba_mmdo/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _4_laba_mmdo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[1].ChartType = SeriesChartType.Point;
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            chart1.Series[0].Points.Clear();
+            double x_min = -2.0;
+            double x_max = 2.0;
+            try
+            {
+                x_min = Convert.ToDouble(numericUpDown1.Text);
+                x_max = Convert.ToDouble(numericUpDown2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Перевірьте поля для вводу, можливо там некоректні дані");
+            }
+            if (x_min > x_max)
+            {
+                MessageBox.Show("Нижня межа не може бути більшою за верхню!!!!");
+            }
+            if (x_max > x_min)
+            {
+                for (double x = x_min; x <= x_max; x += 0.01)
+                {
+                    double y = Function(x);
+                    chart1.Series[0].Points.AddXY(x, y);
+
+                }
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0: MethodLocalPoint(); break;
+                    case 1: Dihotomia(); break;
+                    case 2: GoldSlice(); break;
+                    case 3: Fibonachi(); break;
+                    //case 4: Newton(); break;
+
+                }
+            }
+        }
+
+        private double Function(double x)
+        {
+            return Math.Pow(x, 6) - 3 * Math.Pow(x, 2) + 5 * x + 1;
+        }
+
+        private void MethodLocalPoint()
+        {
+            chart1.Series[1].Points.Clear();
+            double x_min, x_max;
+            double x0 = 1, f0 = Function(x0), x2=0, x1 = 0, h = 0.001, eps = 0.0001;
+            x_min = Convert.ToDouble(numericUpDown1.Value);
+            x_max = Convert.ToDouble(numericUpDown2.Value);
+
+            double f1 = Function(x0), f2 = 0;
+            do
+            {
+                h = h / 2;
+                x2 = x0 + h;
+                f2 = Function(x2);
+                if (f1<=f2)
+                {
+                    h = -h;
+                    x2 = x0;
+                    f2 = Function(f2);
+                }
+
+            } while (f1 <= f2 || Math.Abs(h) < eps);
+
+            if (Math.Abs(h)>eps)
+            {
+                do
+                {
+                    x1 = x2;
+                    f1 = f2;
+                    x2 = x1 + h;
+                    f2 = Function(x2);
+
+                } while (f1<f2);
+            }
+            else
+            {
+
+            }
+
+            if (h > 0)
+            {
+                x_min = x1 - h;
+                x_max = x2;
+            }
+            else
+            {
+                x_min = x1;
+                x_max = x2-h;
+            }
+            
+        }
+
+        private void Dihotomia()
+        {
+            chart1.Series[1].Points.Clear();
+            double x_min, x_max;
+            double eps = 0.0001, d = eps / 3, x1 = 0, x2 = 0, f1 = 0, f2 = 0;
+            x_min = Convert.ToDouble(numericUpDown1.Value);
+            x_max = Convert.ToDouble(numericUpDown2.Value);
+
+            while (x_max - x_min < eps)
+            {
+                x1 = (x_min + x_max - d) / 2;
+                x2 = (x_min + x_max + d) / 2;
+
+                f1 = Function(x1);
+                f2 = Function(x2);
+
+                if (f1 <= f2)
+                {
+                    x_max = x2;
+                }
+                else
+                {
+                    x_max = x1;
+                }
+            }
+            double x_res = (x_min + x_max) / 2, fx_res = Function(x_res);
+            chart1.Series[1].Points.AddXY(x_res, fx_res);
+            chart1.Series[1].Color = Color.Red;
+            x.Text = "Мінімум " + fx_res.ToString() + " 2";
+            chart1.Series[1].MarkerSize = 8;
+        }
+
+        private void GoldSlice()
+        {
+            chart1.Series[1].Points.Clear();
+            double x_min, x_max, eps = 0.0001, u = 0, v = 0, fu = 0, fv = 0;
+            x_min = Convert.ToDouble(numericUpDown1.Value);
+            x_max = Convert.ToDouble(numericUpDown2.Value);
+
+            u = x_min + (3 - Math.Sqrt(5)) / 2 * (x_max - x_min);
+            v = x_min + x_max - u;
+
+            fu = Function(u);
+            fv = Function(v);
+
+            while (x_max - x_min < eps)
+            {
+                if (fu<=fv)
+                {
+                    x_max = v;
+                    v = u;
+                    fv = fu;
+                    u = x_min + x_max - v;
+                    fu = Function(u);
+                }
+                else
+                {
+                    x_min = u;
+                    u = v;
+                    fu = fv;
+                    v = x_min + x_max - u;
+                    fv = Function(v);
+                }
+                if (u > v)
+                {
+                    u = x_min + (3 - Math.Sqrt(5)) / 2 * (x_max - x_min);
+                    v = x_min + x_max - u;
+
+                    fu = Function(u);
+                    fv = Function(v);
+                }
+            }
+            double x_res = (x_min + x_max) / 2, fx_res = Function(x_res);
+            chart1.Series[1].Points.AddXY(x_res, fx_res);
+            chart1.Series[1].Color = Color.Red;
+            x.Text = "Мінімум " + fx_res.ToString()+" 3";
+            chart1.Series[1].MarkerSize = 8;
+        }
+
+        private double F(double n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return F(n - 1) + F(n - 2);
+            }
+        }
+
+        private void Fibonachi()
+        {
+            chart1.Series[1].Points.Clear();
+            double a, b, n=30, u = 0, v = 0, fu = 0, fv = 0;
+            a = Convert.ToDouble(numericUpDown1.Value);
+            b = Convert.ToDouble(numericUpDown2.Value);
+
+            u = a + F(n) / F(n + 2) * (b - a);
+            v = a + b - u;
+
+            fu = Function(u);
+            fv = Function(v);
+
+            for (int i = 0; i < n; i++)
+            {
+                if (fu <= fv)
+                {
+                    b = v;
+                    v = u;
+                    fv = fu;
+                    u = a + b - v;
+                    fu = Function(u);
+                }
+                else
+                {
+                    a = u;
+                    u = v;
+                    fu = fv;
+                    v = a + b - u;
+                    fv = Function(v);
+                }
+
+                if (u > v)
+                {
+                    u = a + F(n - i + 1) / F(n - i + 3) * (b - a);
+                    v = a + b - u;
+                    fu = Function(u);
+                    fv = Function(v);
+                }
+            }
+            double x_res = (a + b) / 2, fx_res = Function(x_res);
+            chart1.Series[1].Points.AddXY(x_res, fx_res);
+            chart1.Series[1].Color = Color.Red;
+            x.Text = "Мінімум " + fx_res.ToString()+" 4";
+            chart1.Series[1].MarkerSize = 8;
+        }
+
+        private void Parabol()
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            chart1.Series.Add("Корінь");
+        }
+    }
+}
